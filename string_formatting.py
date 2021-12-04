ee9204d2ed5bb6b4407ae406d8a271ba818dbce3
Alyssa Cuignet <alyssacuignet@Alyssas-MacBook-Air.local>
2021-12-04 14:10:28 +0000

    exercicses checking vlues list, counting odd and even numbers in  alist, numbered lists and string formatting

---
+++ string_formatting.py
@@ -0,0 +1,21 @@
+# Amend the code below to produce the following message using a "formatted string literal" Colour Selection: Blue, Green
+
+# Colours = ['Red','Blue','Green','Purple']
+# Selection = ___________
+# print(Selection)
+
+Colours = ['Red','Blue','Green','Purple']
+Selection = f"Colour Selection : {Colours[1]}, {Colours[2]}"
+print(Selection)
+
+""" Amend the code below to print "LeafBlue" using the format function
+
+Types = ['Leaf', 'Fire', 'Water']
+Colours = ['Green', 'Red', 'Blue']
+Version1 = ___________
+print(Version1) """
+
+Types = ['Leaf', 'Fire', 'Water']
+Colours = ['Green', 'Red', 'Blue']
+Version1 = "{}{}".format(Types[0],Colours[2])
+print(Version1)s
